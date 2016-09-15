18b89d65802d38b6980f59e8a717735584f66111
Christoph Jansen <jansen.christoph@yahoo.de>
2016-09-15 16:05:31 +0200

    return inspection values in MB

--- container_worker/ic_main.py
+++ container_worker/ic_main.py
@@ -5,9 +5,8 @@ from psutil import virtual_memory
 def main():
     ram = virtual_memory()
     node = {
-        'name': 'local',
-        'total_ram': ram.total,
-        'reserved_ram': ram.total - ram.available,
+        'total_ram': ram.total // (1024 * 1024),
+        'reserved_ram': (ram.total - ram.available) // (1024 * 1024),
         'total_cpus': None,
         'reserved_cpus': None
     }
